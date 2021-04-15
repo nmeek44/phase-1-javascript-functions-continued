7f5f83eae489d4a0773ae18d10443b11001393d5
nmeek44 <nmeek44@gmail.com>
2021-04-15 17:49:12 -0400

    commit tree e4f5e8a29e6e1cdb50d3e6fb31526898edd46412

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        return `This Saturday, I want to roller-skate!`
+    } else {
+        return `This Saturday, I want to ${activity}!`
+    }
+    
+}
+
+function mondayWork(activity) {
+    if (activity === undefined) {
+        return "This Monday, I will go to the office."
+    } else {
+        return `This Monday, I will ${activity}.`
+    }
+}
+
+function wrapAdjective(string) {
+    return function(parameter) {
+        if (parameter === undefined && string === undefined) {
+            `You are *special*!`
+        } else if (parameter === undefined && string === string) {
+            `You are ${string}special${string}!`
+        } else if (parameter === parameter && string === undefined) {
+            `You are *${parameter}*!`
+        } else {
+            `You are ${string}${parameter}${string}!`
+        }
+    }
+}
+const encouragingPromptFunction = wrapAdjective(string)
